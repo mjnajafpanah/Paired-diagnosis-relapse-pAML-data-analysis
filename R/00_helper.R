569a87c3eead06b41d4bf0ecd8a200db3204a983
Mohammad Najafpanah <115652157+mjnajafpanah@users.noreply.github.com>
2026-06-23 11:53:39 -0500

    00_helper.R

---
+++ R/00_helper.R
@@ -0,0 +1,56 @@
+##################################################
+# 00_helper.R
+#
+# Shared packages, helper functions and global data
+##################################################
+
+########################
+# Libraries
+########################
+
+library(Seurat)
+
+library(SingleR)
+
+library(celldex)
+
+library(dplyr)
+
+library(tidyr)
+
+library(data.table)
+
+library(stringr)
+
+library(ggplot2)
+
+library(survival)
+
+library(survminer)
+
+library(Matrix)
+
+library(gtools)
+
+library(reshape2)
+
+library(purrr)
+
+library(dcurves)
+
+library(pheatmap)
+
+
+########################
+# Global options
+########################
+
+options(stringsAsFactors = FALSE)
+
+set.seed(1234)
+
+########################
+# Load helper datasets
+########################
+
+
